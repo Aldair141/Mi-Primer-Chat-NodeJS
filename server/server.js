5ee3e8c493d0d2412daa11eb7b51bac921dad203
Juan Aldair <aldair.rc70@gmail.com>
2020-12-22 10:58:39 -0500

    Primer commit

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
+
+const app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
+
+const rutaPublic = path.resolve(__dirname, '../public');
+const puerto = process.env.PORT || 3000;
+
+app.use(express.static(rutaPublic));
+
+io.on('connection', (client) => {
+    client.broadcast.emit('usuarioConectado', {
+        message: 'Un usuario se conectó a la sala'
+    });
+
+    client.on('enviarMensaje', (data, callback) => {
+        //Emitir mensaje
+        client.broadcast.emit('nuevoMensaje', data);
+
+        callback({
+            ok: true
+        });
+    });
+
+    client.on('disconnect', () => {
+        client.broadcast.emit('usuarioDesconectado', {
+            message: 'Un usuario se desconectó de la sala'
+        });
+    });
+});
+
+server.listen(puerto, (err) => {
+    if (err) throw err;
+    console.log(`Escuchando el puerto ${ puerto }`);
+});
